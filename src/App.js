95a0b122388f0c9cfc1f70b568625a31c94dd8f1
Thorsten Thomann <thorsten.thomann@googlemail.com>
2024-06-24 13:27:24 +0200

    added Kommentar und Beitrag

--- src/App.js
+++ src/App.js
@@ -1,17 +1,36 @@
 import {useState, useEffect} from 'react';
 import {administrator} from "./Benutzer";
-
+import {kommentar, beitrag } from "./Beitrag";
 
 function App() {
 
     const [benutzers, setBenutzers] = useState([administrator]);
+    const [beitraege, setBeitraege] = useState([beitrag]);
+    const [kommentare, setKommentare] = useState([kommentar])
 
+    useEffect(() => {
+        localStorage.setItem('kommentare', JSON.stringify(kommentare));
+    }, [kommentare]);
     useEffect(() => {
         localStorage.setItem('benutzers', JSON.stringify(benutzers));
     }, [benutzers]);
 
     useEffect(() => {
+        localStorage.setItem('beitraege', JSON.stringify(beitraege));
+    }, [beitraege]);
+
+    useEffect(() => {
         const benutzers = JSON.parse(localStorage.getItem('benutzers'));
+        const beitraege = JSON.parse(localStorage.getItem('beitraege'));
+        const kommentare = JSON.parse(localStorage.getItem('kommentare'));
+
+
+        if(kommentare){
+            setKommentare(kommentare)
+        }
+        if(beitraege){
+            setBeitraege(beitraege)
+        }
         if (benutzers) {
             setBenutzers(benutzers);
         }
@@ -21,7 +40,8 @@ function App() {
       <div>
 
           <p>{localStorage.getItem("benutzers")}</p>
-
+          <p>{beitraege[0].inhalt}</p>
+          <p>{localStorage.getItem("kommentare")}</p>
       </div>
   );
 }
---
+++ src/Beitrag.js
@@ -0,0 +1,27 @@
+import {administrator, benutzer} from "./Benutzer";
+
+
+export let kommentar = {
+    "id": "234234",
+    "nutzer": {administrator},
+    "inhalt": "Hallo ich bin ein Kommentar",
+    "datum" : new Date(),
+    "editDatum" : {},
+    "beitragsId" : "1"
+};
+export let beitrag = {
+    "id": "1",
+    "titel" : "Hallo Welt",
+    "nutzer": administrator,
+    "inhalt": "Hallo wie geht es euch?",
+    "publizierungsDatum" : {},
+    "erstellungsDatum" : new Date(),
+    "kommentare" : [kommentar],
+    "kategorien" : ["Neues von der Welt"],
+};
+
+
+
+
+export let beitraege = [];
+export let kommentare = [];
