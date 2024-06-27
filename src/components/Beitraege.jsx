5faa9dbd49b2e4a52ef1e5774a84bb4a5f39d0b3
Thorsten Thomann <thorsten.thomann@googlemail.com>
2024-06-27 15:21:10 +0200

    add Bulma Layout für besseres Design

--- src/components/Beitraege.jsx
+++ src/components/Beitraege.jsx
@@ -36,46 +36,106 @@ export default function Beitraege({aktuellerBenutzer,
         setIstAltZuNeu(!istAltZuNeu);
     };
     return(
-        <div
-            style={{
-                backgroundColor: "lightblue",
-                display: "flex",
-                flexDirection:"column",
-                borderRadius: "20px"
-            }}
-        >
-            <div
-                style={{
-                    textAlign: "right"
-                }}
-            >
-                {aktuellerBenutzer
-                    &&  aktuellerBenutzer.rolle.kannBeitragVerfassen
-                    && <button onClick={hantiereClick}> ➕ Beitrag erstellen</button>
-                }
-                <button
-                    onClick={hantiereClickAltZuNeu}
-                >
-                    {(istAltZuNeu ? "⬆️" : "⬇️")}
-                </button>
+        <>
+            {/*<div*/}
+            {/*    style={{*/}
+            {/*        backgroundColor: "lightblue",*/}
+            {/*        display: "flex",*/}
+            {/*        flexDirection: "column",*/}
+            {/*        borderRadius: "20px"*/}
+            {/*    }}*/}
+            {/*>*/}
+            {/*    <div*/}
+            {/*        style={{*/}
+            {/*            textAlign: "right"*/}
+            {/*        }}*/}
+            {/*    >*/}
+            {/*        {aktuellerBenutzer*/}
+            {/*            && aktuellerBenutzer.rolle.kannBeitragVerfassen*/}
+            {/*            &&*/}
+            {/*            <button onClick={hantiereClick}><i className="fa fa-save"></i> {" "} Beitrag erstellen</button>*/}
+            {/*        }*/}
+            {/*        <button*/}
+            {/*            onClick={hantiereClickAltZuNeu}*/}
+            {/*            style={{*/}
+            {/*                marginRight: "2em",*/}
+            {/*            }}*/}
+            {/*        >*/}
+            {/*            {(istAltZuNeu ? "⬆️" : "⬇️")}*/}
+            {/*        </button>*/}
+            {/*    </div>*/}
+
+            {/*    {geklickt && <BeitragErstellenFormular beitraege={beitraege}*/}
+            {/*                                           setBeitraege={setBeitraege}*/}
+            {/*                                           aktuellerBenutzer={aktuellerBenutzer}/>}*/}
+
+            {/*    {beitraege.sort(compareDates).map(beitrag =>*/}
+            {/*        <Beitrag key={beitrag.id}*/}
+            {/*                 beitrag={beitrag}*/}
+            {/*                 beitraege={beitraege}*/}
+            {/*                 setBeitraege={setBeitraege}*/}
+            {/*                 kommentare={kommentare}*/}
+            {/*                 setKommentare={setKommentare}*/}
+            {/*                 aktuellerBenutzer={aktuellerBenutzer}*/}
+            {/*        />)*/}
+            {/*    }*/}
+
+
+            {/*</div>*/}
+            <div className="container"  >
+                <div className="section"    >
+                    <div className="columns">
+                        <div className="columns has-text-centered">
+                            <h2
+                                className="title"
+                                style={{
+                                    color: "ghostwhite",
+                                    marginRight: "20em",
+                            }}> Beiträge </h2>
+                            <br />
+                        </div>
+                    </div>
+                    <div style={{
+                        textAlign: "right",
+                        marginRight: "10em"
+                    }}>
+                    {aktuellerBenutzer
+                        && aktuellerBenutzer.rolle.kannBeitragVerfassen
+                        &&
+                        <button
+                            onClick={hantiereClick}
+                                className="button is-info"><i className="fa fa-save"></i> {" "} Beitrag erstellen</button>
+                    }
+                    <button
+                        onClick={hantiereClickAltZuNeu}
+                        className="button is-info"
+                    >
+                        {(istAltZuNeu ? "⬆️" : "⬇️")}
+                    </button>
+
+
+                        {geklickt && <BeitragErstellenFormular beitraege={beitraege}
+                                                       setBeitraege={setBeitraege}
+                                                       aktuellerBenutzer={aktuellerBenutzer}/>}
+
+                    </div>
+                    <div id="app"
+                         className="row columns is-multiline is-centered"
+                         >
+                            {beitraege.sort(compareDates).map(beitrag =>
+                                <Beitrag key={beitrag.id}
+                                         beitrag={beitrag}
+                                         beitraege={beitraege}
+                                         setBeitraege={setBeitraege}
+                                         kommentare={kommentare}
+                                         setKommentare={setKommentare}
+                                         aktuellerBenutzer={aktuellerBenutzer}
+                                />)
+                            }
+                    </div>
+
+                </div>
             </div>
-
-            {geklickt && <BeitragErstellenFormular beitraege={beitraege}
-                                                   setBeitraege={setBeitraege}
-                                                   aktuellerBenutzer={aktuellerBenutzer} />}
-
-            {beitraege.sort(compareDates).map(beitrag =>
-                                    <Beitrag key={beitrag.id}
-                                             beitrag={beitrag}
-                                             beitraege={beitraege}
-                                             setBeitraege={setBeitraege}
-                                             kommentare={kommentare}
-                                             setKommentare={setKommentare}
-                                             aktuellerBenutzer={aktuellerBenutzer}
-                                                                                    />)
-            }
-
-
-        </div>
+        </>
     );
 }
