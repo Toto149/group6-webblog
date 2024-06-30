2c342b25c2aea7d74b9de473b68d70e8f0250a57
SchroederErwin <schroedererwin97@gmail.com>
2024-06-30 14:21:50 +0200

    Kleiner Typo Fix

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,8 +5,7 @@ export default function (){
             <div className="content has-text-centered">
                 <p className="has-text-white">
                     This website has been created by <a href="https://github.com/OlenaVoznenko">Olena Voznenko</a>, <a
-                    href="https://github.com/SchroederErwin">Erwin Schroeder</a> and <a href="https://github.com/Toto149">Thorsten Thomann</a>
-                    >.
+                    href="https://github.com/SchroederErwin">Erwin Schroeder</a> and <a href="https://github.com/Toto149">Thorsten Thomann</a>.
                 </p>
             </div>
         </footer>
